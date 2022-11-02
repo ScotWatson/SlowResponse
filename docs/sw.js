ddaee0eda9ef7f37526f0f1259f2f9922b6f553e
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-11-02 18:18:38 -0400

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -3,7 +3,7 @@
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-
+"use strict";
 
 function project_file(filename) {
   const pathname = self.registration.scope;
@@ -129,12 +129,12 @@ self.addEventListener("pushsubscriptionchange", selfPushSubscriptionChange);
 self.addEventListener("sync", selfSync);
 
 function registrationUpdateFound() {
+  console.log(self.registration.waiting);
+  console.log(self.registration.active);
+  console.log(self.registration.installing);
+  console.log(self.registration.updateViaCache);
 }
 
-  self.registration.waiting
-  self.registration.active
-  self.registration.installing
-  self.registration.updateViaCache
 /*
   self.registration.index
   self.registration.navigationPreload
@@ -144,5 +144,5 @@ function registrationUpdateFound() {
   getNotifications()
   showNotification()
 */
-  unregister()
-  update()
+//  unregister()
+//  update()
